b0e56571fe5a926b229d48aa7326d89afddd7b02
rahulsrajput <rahulsrajput29@gmail.com>
2024-04-24 20:24:34 +0530

    CSRF_TRUSTED_ORIGINS

--- TrueFeedback/settings.py
+++ TrueFeedback/settings.py
@@ -151,4 +151,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://truefeedback-rsr.vercel.app/"]
