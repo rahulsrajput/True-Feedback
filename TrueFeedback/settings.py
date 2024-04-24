6385ef3773c1b5d6b6e70fd16df1f7c0b10da9f2
rahulsrajput <rahulsrajput29@gmail.com>
2024-04-24 20:13:17 +0530

    database changed"

--- TrueFeedback/settings.py
+++ TrueFeedback/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'TrueFeedback.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
     }
 }
 
